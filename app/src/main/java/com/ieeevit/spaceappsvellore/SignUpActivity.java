354a42962d7ec5d3e214f1a36f3899619939b3fa
Karishnu Poddar <karishnu@gmail.com>
2017-04-20 00:33:12 +0530

    ui

--- app/src/main/java/com/ieeevit/spaceappsvellore/SignUpActivity.java
+++ app/src/main/java/com/ieeevit/spaceappsvellore/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.ieeevit.spaceappsvellore;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this,R.color.colorPrimary));
     }
 }
