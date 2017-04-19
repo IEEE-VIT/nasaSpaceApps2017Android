39c6419dc445b50af42b9883018bb923ae7041e3
Karishnu Poddar <karishnu@gmail.com>
2017-04-19 23:47:37 +0530

    utilities

---
+++ app/src/main/java/com/ieeevit/spaceappsvellore/models/SignUp.java
@@ -0,0 +1,87 @@
+
+package com.ieeevit.spaceappsvellore.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SignUp {
+
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("contact")
+    @Expose
+    private String contact;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("college")
+    @Expose
+    private String college;
+    @SerializedName("location")
+    @Expose
+    private String location;
+    @SerializedName("skill")
+    @Expose
+    private String skill;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+}
---
+++ app/src/main/java/com/ieeevit/spaceappsvellore/utility/Preferences.java
@@ -0,0 +1,28 @@
+package com.ieeevit.spaceappsvellore.utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Karishnu Poddar on 28/08/2016.
+ */
+public class Preferences {
+    public static void setPrefs(String key, String value, Context context){
+        SharedPreferences sharedpreferences = context.getSharedPreferences("IEEEPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getPrefs(String key, Context context){
+        SharedPreferences sharedpreferences = context.getSharedPreferences("IEEEPreferences", Context.MODE_PRIVATE);
+        return sharedpreferences.getString(key, "not_found");
+    }
+
+    public static void clearPrefs(Context context){
+        SharedPreferences sharedpreferences = context.getSharedPreferences("IEEEPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.clear();
+        editor.apply();
+    }
+}
