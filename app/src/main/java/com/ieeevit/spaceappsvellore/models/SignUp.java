ed10f5de9c89196ada5c3d1563cd4bbf9f3bad4e
Mayur Shastri <mayurshastri97@gmail.com>
2017-04-29 02:33:27 +0530

    profile connection

--- app/src/main/java/com/ieeevit/spaceappsvellore/models/SignUp.java
+++ app/src/main/java/com/ieeevit/spaceappsvellore/models/SignUp.java
@@ -5,81 +5,143 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class SignUp implements Serializable{
 
-    @SerializedName("email")
+    @SerializedName("time_sign_up")
     @Expose
-    private String email;
-    @SerializedName("password")
+    private String timeSignUp;
+    @SerializedName("lunch1")
     @Expose
-    private String password;
-    @SerializedName("contact")
+    private Boolean lunch1;
+    @SerializedName("lunch2")
     @Expose
-    private String contact;
-    @SerializedName("name")
+    private Boolean lunch2;
+    @SerializedName("dinner")
     @Expose
-    private String name;
-    @SerializedName("college")
+    private Boolean dinner;
+    @SerializedName("admin")
     @Expose
-    private String college;
-    @SerializedName("location")
+    private Boolean admin;
+    @SerializedName("attendance")
     @Expose
-    private String location;
+    private Boolean attendance;
+    @SerializedName("activation_token")
+    @Expose
+    private String activationToken;
+    @SerializedName("component_req")
+    @Expose
+    private List<Object> componentReq = null;
+    @SerializedName("component_given")
+    @Expose
+    private List<Object> componentGiven = null;
+    @SerializedName("__v")
+    @Expose
+    private Integer v;
     @SerializedName("skill")
     @Expose
     private String skill;
-
-    @SerializedName("activation_token")
+    @SerializedName("college")
     @Expose
-    public String activation_token;
+    private String college;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("wifi_username")
+    @Expose
+    private String wifiUsername;
+    @SerializedName("wifi_password")
+    @Expose
+    private String wifiPassword;
+    @SerializedName("_id")
+    @Expose
+    private String id;
 
-    public String getEmail() {
-        return email;
+    public String getTimeSignUp() {
+        return timeSignUp;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setTimeSignUp(String timeSignUp) {
+        this.timeSignUp = timeSignUp;
     }
 
-    public String getPassword() {
-        return password;
+    public Boolean getLunch1() {
+        return lunch1;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setLunch1(Boolean lunch1) {
+        this.lunch1 = lunch1;
     }
 
-    public String getContact() {
-        return contact;
+    public Boolean getLunch2() {
+        return lunch2;
     }
 
-    public void setContact(String contact) {
-        this.contact = contact;
+    public void setLunch2(Boolean lunch2) {
+        this.lunch2 = lunch2;
     }
 
-    public String getName() {
-        return name;
+    public Boolean getDinner() {
+        return dinner;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setDinner(Boolean dinner) {
+        this.dinner = dinner;
     }
 
-    public String getCollege() {
-        return college;
+    public Boolean getAdmin() {
+        return admin;
     }
 
-    public void setCollege(String college) {
-        this.college = college;
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
     }
 
-    public String getLocation() {
-        return location;
+    public Boolean getAttendance() {
+        return attendance;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+    public void setAttendance(Boolean attendance) {
+        this.attendance = attendance;
+    }
+
+    public String getActivationToken() {
+        return activationToken;
+    }
+
+    public void setActivationToken(String activationToken) {
+        this.activationToken = activationToken;
+    }
+
+    public List<Object> getComponentReq() {
+        return componentReq;
+    }
+
+    public void setComponentReq(List<Object> componentReq) {
+        this.componentReq = componentReq;
+    }
+
+    public List<Object> getComponentGiven() {
+        return componentGiven;
+    }
+
+    public void setComponentGiven(List<Object> componentGiven) {
+        this.componentGiven = componentGiven;
+    }
+
+    public Integer getV() {
+        return v;
+    }
+
+    public void setV(Integer v) {
+        this.v = v;
     }
 
     public String getSkill() {
@@ -90,9 +152,59 @@ public class SignUp implements Serializable{
         this.skill = skill;
     }
 
-    public String getActivation_token(){return activation_token;}
+    public String getCollege() {
+        return college;
+    }
 
-    public void setActivation_token(String activation_token){
-        this.activation_token = activation_token;
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWifiUsername() {
+        return wifiUsername;
+    }
+
+    public void setWifiUsername(String wifiUsername) {
+        this.wifiUsername = wifiUsername;
+    }
+
+    public String getWifiPassword() {
+        return wifiPassword;
+    }
+
+    public void setWifiPassword(String wifiPassword) {
+        this.wifiPassword = wifiPassword;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
